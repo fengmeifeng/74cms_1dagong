5b38e896d40f86a186955036455c1927f888c9fa
fengmeifeng <761450221@qq.com>
2019-11-19 14:24:26 +0800

    提交

---
+++ s/plus/ajax_common.php
@@ -0,0 +1,5 @@
+<?php
+/*74cms·ÖÕ¾ÎÄ¼þ*/
+$_GET['subsite_id']=8;
+require dirname(__FILE__).'/../../plus/'.basename(__FILE__);
+?>
---
+++ sh/user/registered_count_ah.php
@@ -0,0 +1,5 @@
+<?php
+/*74cms·ÖÕ¾ÎÄ¼þ*/
+$_GET['subsite_id']=5;
+require dirname(__FILE__).'/../../user/'.basename(__FILE__);
+?>
